683b0b97b8e5ff8d5f02e1c174f7477121c2208e
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2025-02-28 16:57:08 +0000

    TF ready

--- mirkitten/TF.py
+++ mirkitten/TF.py
@@ -1,11 +1,12 @@
 import os
 import pandas as pd
-import decouple as dc
-
+import decoupler as dc
+import yaml
 
 
 class TranFact():
-    def __init__(self, dds_dict, pvalue=0.05, threshold=None, interest='stat', species='human'):
+    def __init__(self, dds_files_path, pvalue=0.05, threshold=None, interest='stat', species='human'):
+        dds_dict = self._load_dds_files(dds_files_path=dds_files_path)
         self.dds_dict = dds_dict
         self.pvalue = pvalue
         self.threshold = threshold
@@ -13,6 +14,14 @@ class TranFact():
         self.colletri = self.set_colletri(species=species)
         self.mats = {}
         self.tf_dfs = {}
+    def _load_dds_files(self, dds_files_path):
+        """
+        Loads DDS files from a YAML file and returns a dictionary of DataFrames.
+        """
+        with open(dds_files_path, 'r') as file:
+            dds_dict = yaml.safe_load(file)
+        return {dds: pd.read_csv(file, index_col=0) for dds, file in dds_dict.items()}
+    
     def set_colletri(self, species='human'):
         if species=='human' and "collectri" in os.listdir('/home/amore/work/data'):
             collectri = pd.read_csv('collectri.csv')
@@ -21,44 +30,56 @@ class TranFact():
         self.collectri = collectri
         return collectri
 
-    def get_mat(self, dds, interest='stat', comparison='comparison', experiment='experiment'):
+    def get_mat(self, dds, interest='stat', comparison='comparison'):
         """
         This function will take a dataframe with the dds results and will return a dataframe with the DE genes
         """
-        if f'mat_{experiment}_{comparison}.csv' in os.listdir('results'):
-            mat = pd.read_csv(f'results/mat_{experiment}_{comparison}.csv', index_col=0)
+        if comparison in self.mats:
+            mat = self.mats[comparison]
+        if f'mat_{comparison}.csv' in os.listdir('results'):
+            mat = pd.read_csv(f'results/mat_{comparison}.csv', index_col=0)
             mat = mat.T
         else:
             mat = dds[[interest]].T.rename(index={interest: comparison})
-            mat.T.to_csv(f'results/mat_{experiment}_{comparison}.csv')
-        self.mat[comparison]= mat
+            
+            try:
+                mat.T.to_csv(f'results/mat_{comparison}.csv')
+            except OSError:
+                print(f"coudn't save at results/mat_{comparison}.csv. Currently at {os.system('pwd')}")
+
+        self.mats[comparison]= mat
         return mat
-    def get_tf_df(self, mat, experiment='experiment', comparison='comparison'):
-        if self.tf_dfs[comparison] is not None:
+    def get_tf_df(self, mat, comparison='comparison'):
+        if comparison in self.tf_dfs and self.tf_dfs[comparison] is not None:
             tf_df = self.tf_dfs[comparison]
-        elif f'tf_acts_{experiment}_{comparison}.csv' in os.listdir('results'):
-            tf_df = pd.read_csv(f'results/tf_acts{experiment}_{comparison}.csv', index_col=0)
+        elif f'tf_acts_{comparison}.csv' in os.listdir('results'):
+            tf_df = pd.read_csv(f'results/tf_acts_{comparison}.csv', index_col=0)
             self.tf_dfs[comparison] = tf_df
         else:
             tf_acts, tf_pvals = dc.run_ulm(mat=mat, net=self.collectri, verbose=True)
             tf_df = pd.DataFrame(tf_acts.T)
             tf_df['pvals']=tf_pvals.T
-            tf_df.to_csv(f'results/tf_acts{experiment}_{comparison}.csv')
+            try:
+                tf_df.to_csv(f'results/tf_acts_{comparison}.csv')
+            except OSError:
+                print(f"coudn't save at rresults/tf_acts_{comparison}.csv. Currently at {os.system('pwd')}")
             self.tf_dfs[comparison] = tf_df
         return tf_df
 
-    def get_tf_df_of_comparison(self, dds, interest='stat', comparison='comparison', experiment='experiment'):
+    def get_tf_df_of_comparison(self, dds, interest='stat', comparison='comparison'):
         """
         This function will take a dataframe with the dds results and will return a dataframe with the DE genes
         """
-        mat = self.set_mat(dds=dds, interest=interest, comparison=comparison, experiment=experiment)
-        tf_df = self.get_tf_df(mat=mat, experiment=experiment, comparison=comparison)
+        mat = self.get_mat(dds=dds, interest=interest, comparison=comparison)
+        tf_df = self.get_tf_df(mat=mat, comparison=comparison)
         return tf_df
-    def get_tf_df_of_combined_comparisons(self, experiment='experiment'):
+    def get_tf_df_of_combined_comparisons(self):
         all_tf_dic = {}
         for comparison, dds in self.dds_dict.items():
-            tf_df = self.get_tf_df_of_comparison(dds=dds, comparison=comparison, experiment=experiment)
-            all_tf_dic[comparison] = tf_df
+            tf_df = self.get_tf_df_of_comparison(dds=dds, comparison=comparison)
+            tf_df = tf_df[tf_df['pvals']<self.pvalue]
+            all_tf_dic[comparison] = tf_df[comparison]
+            #print( tf_df.head())
         all_scores_tf_df=None
         for comparison, score in all_tf_dic.items():
             if all_scores_tf_df is None:
@@ -68,9 +89,14 @@ class TranFact():
         all_scores_tf_df.fillna(0, inplace=True)
 
         return all_scores_tf_df
+        
+        if isinstance(genes, pd.Series):  # If multiple values exist
+            gene_list.append(genes.tolist())
+        else:  # Single value case
+            gene_list.append([genes])
     
-    def save_tf_df_of_combined_comparisons(self, experiment='experiment', path='results/tf_df_combined.csv'):
-        tf_df = self.get_tf_df_of_combined_comparisons(experiment=experiment)
+    def save_tf_df_of_combined_comparisons(self, path='results/tf_df_combined.csv'):
+        tf_df = self.get_tf_df_of_combined_comparisons()
         tf_df.to_csv(path)
         
     def get_enrtiched_tf(self, comparison='comparison', pvalue=0.1):
--- mirkitten/run_TF.py
+++ mirkitten/run_TF.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run DEA')
     parser.add_argument('--dds_files', type=str, help='YAML file with the names of the comparisons and the path to the DDS file')
     parser.add_argument('--save_name', type=str, help='Path to save the results')
-    parser.add_argument('--pvalue', type=float, help='Pvalue to filter the results')
-    parser.add_argument('--threshold', type=float, help='Threshold to filter the results')
-    parser.add_argument('--interest', type=str, help='Interest to filter the results')
-    parser.add_argument('--species', type=str, help='Species to filter the results')
+    parser.add_argument('--pvalue', type=float, help='Pvalue to filter the results', default=0.05)
+    parser.add_argument('--threshold', type=float, help='Threshold to filter the results', default=None)
+    parser.add_argument('--interest', type=str, help='Interest to filter the results', default='stat')
+    parser.add_argument('--species', type=str, help='Species to filter the results', default='human')
 
     args = parser.parse_args()
     if args.pvalue is not None:
@@ -33,6 +33,6 @@ if __name__ == '__main__':
             raise ValueError('Interest must be in ["stat", "log2FoldChange"]')
     else:
         args.interest = 'stat'
-    tf = TF.TF(dds_files_path=args.dds_files, pvalue=args.pvalue, threshold=args.threshold, interest=args.interest, species=args.species)
+    tf = TF.TranFact(dds_files_path=args.dds_files, pvalue=args.pvalue, threshold=args.threshold, interest=args.interest, species=args.species)
     tf.save_tf_df_of_combined_comparisons(args.save_name)
     print('Done')
